db66f0779b56a4af0c7cf5707b912ded2e5f892e
David Johansson <davidlgjohansson@gmail.com>
2023-07-14 12:55:30 +0200

    added custom printer function that defines message length and terminates with \r

--- Kallebol Elevation.py
+++ Kallebol Elevation.py
@@ -46,10 +46,10 @@ def Encoder_callback(args):
     global encoder_value
     if ENC_B.value() == 1:
         encoder_value = encoder_value + 1
-        print("I increesed the counter to : " + str(encoder_value))
+        print_custom("I increesed the counter to : " + str(encoder_value))
     else:
         encoder_value = encoder_value - 1
-        print("I DEcreesed the counter to : " + str(encoder_value))
+        print_custom("I DEcreesed the counter to : " + str(encoder_value))
     time.sleep_ms(1) #debounce
     update_elevation_stage_direction()
         
@@ -58,7 +58,7 @@ def homing_callback(args):
     global encoder_value
     global homeing
     global setpoint_value
-    print("homeswitch reached")
+    print_custom("homeswitch reached")
     encoder_value = 0
     setpoint_value = 0
     homeing = 0
@@ -67,7 +67,7 @@ def homing_callback(args):
 
 def limit_callback(args): # this should do something to ensure that it does not pass the limit
     global HW_stop
-    print("limit switch reached")
+    print_custom("limit switch reached")
     update_stops()
     update_elevation_stage_direction()
         
@@ -136,40 +136,44 @@ def Elevation_command_handler(data):
         try:
             setpoint_value = int(data[2])
         except Exception as e:
-            print(e)
-            print(data[3])
+            foo = []
+            foo[0] = e
+            foo[1] =data[3]
+            print_custom(foo)
     if (data[1] == "SET"):
         try:
             encoder_value = int(data[2])
         except Exception as e:
-            print(e)
+            print_custom(e)
     if (data[1] == "STOP"):
         stop = 1
         if (data[2] == "RESET"):
             stop = 0
     if (data[1] == "GET"):
         if (data[2] == "POSITION"):
-            print(str(encoder_value))
+            print_custom(str(encoder_value))
         if (data[2] == "TARGET"):
-            print(str(setpoint_value))
+            print_custom(str(setpoint_value))
         if (data[2] == "FLAGS"):
-            print("Homeing : " + str(homeing) + " ,Movedir : " + str(movedir) + " ,HWmovedir : " + str(hardware_movedir) + " ,Stop : " + str(stop) + " ,HW_stop : " + str(HW_stop))
+            print_custom("Homeing : " + str(homeing) + " ,Movedir : " + str(movedir) + " ,HWmovedir : " + str(hardware_movedir) + " ,Stop : " + str(stop) + " ,HW_stop : " + str(HW_stop))
         if (data[2] == "PINS"):
-            print("LOAD1 : " + str(LOAD1_pin.value()) +  " ,LOAD2 : " + str(LOAD2_pin.value()) + " ,ENC_A : " + str(ENC_A.value()) + " ,ENC_B : " + str(ENC_B.value()))
-            print("Homing_pin : " + str(Homing_pin.value())  + " ,Limit_pin : " + str(Limit_pin.value()))
-            print("RF1_pin " + str(RF1_pin.value()) + " ,RF2_pin " + str(RF2_pin.value())+ " ,RF3_pin " + str(RF3_pin.value())+ " ,RF4_pin " + str(RF4_pin.value()))
-            print("Hbridge1 : " + str(H_bridge_1.value())  +  "Hbridge2 : " + str(H_bridge_2.value()))
+            foo = []
+            foo [0] = "LOAD1 : " + str(LOAD1_pin.value()) +  " ,LOAD2 : " + str(LOAD2_pin.value()) + " ,ENC_A : " + str(ENC_A.value()) + " ,ENC_B : " + str(ENC_B.value())
+            foo [1] = "Homing_pin : " + str(Homing_pin.value())  + " ,Limit_pin : " + str(Limit_pin.value())
+            foo [2] = "RF1_pin " + str(RF1_pin.value()) + " ,RF2_pin " + str(RF2_pin.value())+ " ,RF3_pin " + str(RF3_pin.value())+ " ,RF4_pin " + str(RF4_pin.value())
+            foo [3] = "Hbridge1 : " + str(H_bridge_1.value())  +  "Hbridge2 : " + str(H_bridge_2.value())
+            print_custom(foo)
         if (data[2] ==  "ALL"):
-            print("=====variables=====")
-            print("Current position = " + str(encoder_value))
-            print("Target position = " + str(setpoint_value))
-            print("Homeing : " + str(homeing) + " ,Movedir : " + str(movedir) + " ,HWmovedir : " + str(hardware_movedir) + " ,Stop : " + str(stop) + " ,HW_stop : " + str(HW_stop))
-            print("=====pins=====")
-            print("LOAD1 : " + str(LOAD1_pin.value()) +  " ,LOAD2 : " + str(LOAD2_pin.value()) + " ,ENC_A : " + str(ENC_A.value()) + " ,ENC_B : " + str(ENC_B.value()))
-            print("Homing_pin : " + str(Homing_pin.value())  + " ,Limit_pin : " + str(Limit_pin.value()))
-            print("RF1_pin " + str(RF1_pin.value()) + " ,RF2_pin " + str(RF2_pin.value())+ " ,RF3_pin " + str(RF3_pin.value())+ " ,RF4_pin " + str(RF4_pin.value()))
-            print("Hbridge1 : " + str(H_bridge_1.value())  +  " Hbridge2 : " + str(H_bridge_2.value()))
-    
+            foo [0] = "=====variables====="
+            foo [1] = "Current position = " + str(encoder_value)
+            foo [2] = "Target position = " + str(setpoint_value)
+            foo [3] = "Homeing : " + str(homeing) + " ,Movedir : " + str(movedir) + " ,HWmovedir : " + str(hardware_movedir) + " ,Stop : " + str(stop) + " ,HW_stop : " + str(HW_stop)
+            foo [4] = "=====pins====="
+            foo [5] = "LOAD1 : " + str(LOAD1_pin.value()) +  " ,LOAD2 : " + str(LOAD2_pin.value()) + " ,ENC_A : " + str(ENC_A.value()) + " ,ENC_B : " + str(ENC_B.value())
+            foo [6] = "Homing_pin : " + str(Homing_pin.value())  + " ,Limit_pin : " + str(Limit_pin.value())
+            foo [7] = "RF1_pin " + str(RF1_pin.value()) + " ,RF2_pin " + str(RF2_pin.value())+ " ,RF3_pin " + str(RF3_pin.value())+ " ,RF4_pin " + str(RF4_pin.value())
+            foo [8] = "Hbridge1 : " + str(H_bridge_1.value())  +  "Hbridge2 : " + str(H_bridge_2.value())
+            print_custom(foo)
         
     
 
@@ -187,11 +191,11 @@ def command_interpreter(data):
     elif (splitdata[0] == "LOAD"):
         LOAD_handler(splitdata)
     elif (splitdata[0] == "RFSWITCH"):
-        print("to be implemented")
-    elif (splitdata[0] == "ELEVATION" or plitdata[0] == "EL"):
+        print_custom("to be implemented")
+    elif (splitdata[0] == "ELEVATION" or splitdata[0] == "EL"):
         Elevation_command_handler(splitdata)
     elif (splitdata[0] == "EXPANTION"):
-        print("to be implemented")
+        print_custom("to be implemented")
         
     update_elevation_stage_direction()
     
@@ -270,25 +274,59 @@ def update_stops():
         homeing = 0
         HW_stop = -1
     if Homing_pin.value() == homing_pin_polarity and Limit_pin.value() == limit_pin_polarity:
-        print("ERROR both limit switches appear to have triggered at the same time")
-        print("ERROR I have pulled the E-stop, please reset before resuming operation")
+        foo = []
+        foo[0] = "ERROR both limit switches appear to have triggered at the same time"
+        foo[1] = "ERROR I have pulled the E-stop, please reset before resuming operation"
+        print_custom(foo)
         stop = 1
 
 
+def print_custom(printdata):
+    # This function takes a array of stings as input and prints it
+    # A singular array works aswell
+    length = 0
+    length_str = ""
+    try:
+        if type(printdata) == type("foo"):
+            # if the input is a string
+            length = len(printdata) + 5 # the five extra chars are the length header and the carrige return
+            if length < 9: # I deem printF as black magic and refuse to use it
+                length_str = "00" + str(length)
+            elif length < 99:
+                length_str = "0" + str(length)
+                
+            print(length_str +  " " + printdata, end="\n\r")
+        else:
+           # if the inputdata is an array
+            for st in printdata:
+                length = length + len(st)
+            length = length + 5
+            if length < 9: # I deem printF as black magic and refuse to use it
+                length_str = "00" + str(length)
+            elif length < 99:
+                length_str = "0" + str(length)
+            print(length_str, end=" ") #Print witout newline, but with explicit space
+            for st in printdata:
+                if st == printdata[-1]:
+                    print(st, end="\n\r")
+                else:
+                    print(st, end="\n")
+    except Exception as e:
+        print("035 ERROR in customprint function\r")
 
 #enable IRQs
 ENC_A.irq(trigger=ENC_A.IRQ_FALLING, handler=Encoder_callback)
 Homing_pin.irq(trigger=Homing_pin.IRQ_RISING, handler=homing_callback)
 Limit_pin.irq(trigger=Limit_pin.IRQ_RISING, handler=limit_callback)
 #Infinite loop of the program
-print("booted")
+print_custom ("booted the MCU")
 while True:
     update_stops()
     poll_results = poll_obj.poll(10)
     if poll_results:
         data = sys.stdin.readline().strip()
         if data:
-            print("received data: ", data)
+            print_custom(["received data: ", data])
             command_interpreter(data)
     
 
